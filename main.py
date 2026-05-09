a4d6ae0e7d24e0daa4f38bb06080618da8c762ee
Qianyi Li <Yunjie Lai>
2026-05-09 17:35:52 +1000

    Completed booking flow and event pages

--- main.py
+++ main.py
@@ -2,9 +2,42 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "<h1>Hello AS2</h1>"
 
-if __name__ == "__main__":
+# Home Page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Event Details Page
+@app.route('/details')
+def details():
+    return render_template('event-details.html')
+
+
+# Create / Update Event Page
+@app.route('/create')
+def create_event():
+    return render_template('create-update-event.html')
+
+
+# Booking Page
+@app.route('/booking')
+def booking():
+    return render_template('booking.html')
+
+
+# Booking History Page
+@app.route('/bookings')
+def bookings():
+    return render_template('booking-history.html')
+
+
+# Login / Register Page
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
